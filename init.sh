8585f23b0952a8a995056062ab792324ccdd23cc
saintkim12 <saintkim1232@gmail.com>
2025-07-27 15:28:39 +0900

    init services 27

--- init.sh
+++ init.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -e
 
-SKIP_SERVICES="duckdns/"
+GIT_USER=saintkim12
+GIT_REPO=onyu-home-main-vm
+GIT_BRANCH=main
+GIT_URL=https://github.com/${GIT_USER}/${GIT_REPO}.git
+MAIN_VM_DIR=/opt/setup/main-vm
+YOUR_SERVER_IP='<your-server-ip>'
 
-# Git 저장소 clone
-# git clone https://github.com/saintkim12/onyu-home-main-vm.git /opt/setup
 mkdir -p /opt/setup
 cd /opt/setup
 
-# 주입된 .env 파일 불러오기
-. ./.env
-
-# git, docker 설치
-echo "🔧 Installing base packages..."
+### [1] Alpine 패키지 및 Docker 설치
+echo "📦 Installing Docker..."
 apk update
-apk add --no-cache git docker docker-compose openrc curl
+apk add --no-cache docker docker-compose git curl openrc
 
 echo "🔌 Enabling docker service..."
 rc-update add docker boot
@@ -22,25 +22,28 @@ service docker start
 
 echo "📥 Cloning Git repository..."
 
+### [2] Git 저장소 클론
 if [ ! -d "./main-vm" ]; then
-  git clone -b "$GIT_BRANCH" "$GIT_URL" /opt/setup/main-vm
+  git clone -b "$GIT_BRANCH" "$GIT_URL" MAIN_VM_DIR
 else
   echo "📦 Repo exists, pulling latest..."
-  cd "main-vm" && git pull && cd ..
+  cd $MAIN_VM_DIR && git pull && cd ..
 fi
-cd main-vm/docker
 
-echo "🚀 Starting all Docker services..."
+### [3] 메인 디렉토리로 이동
+cd $MAIN_VM_DIR
 
-for dir in */ ; do
-  if echo "$SKIP_SERVICES" | grep -q "$dir"; then
-    echo "⏭️ Skipping $dir"
-    continue
-  fi
-  if [ -f "$dir/docker-compose.yaml" ]; then
-    echo "🟢 Launching $dir"
-    (cd "$dir" && docker-compose --env-file /opt/setup/.env up -d)
-  fi
-done
+### [4] Portainer Docker 컨테이너 실행
+echo "🚀 Starting Portainer..."
+cd portainer
+export PORTAINER_PORT=8100
+docker-compose up -d
 
-echo "✅ All services launched."
+echo "✅ Portainer started on :$PORTAINER_PORT"
+echo "👉 Access at: http://$YOUR_SERVER_IP:$PORTAINER_PORT"
+
+echo ""
+echo "📝 Next Step:"
+echo "1. Open Portainer UI"
+echo "2. Set Stack and Run Container"
+
